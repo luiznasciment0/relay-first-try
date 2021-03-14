dcedeae7481a21d284ac33afbd84291cbc0d0b5f
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-14 19:37:28 -0300

    a

--- src/fetchGraphQL.js
+++ src/fetchGraphQL.js
@@ -1,6 +1,5 @@
 const fetchGraphQL = async (text,variables) => {
-    const REACT_APP_GITHUB_AUTH_TOKEN = '4e5acdcafd78cc9ba797907d406423702469a0c5'
-    console.log('REACT_APP_GITHUB_AUTH_TOKEN', REACT_APP_GITHUB_AUTH_TOKEN)
+    const REACT_APP_GITHUB_AUTH_TOKEN = process.env.REACT_APP_GITHUB_AUTH_TOKEN
     const response = await fetch('https://api.github.com/graphql', {
         method: 'POST',
         headers: {
